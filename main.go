fea55f01de6a34f10022be0ef83a5e8889faca42
Akio <97880708+YoruAkio@users.noreply.github.com>
2024-08-16 20:54:24 +0000

    chore: Update main.go to use logger for printing configuration

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/yoruakio/gowebserver/config"
+	"github.com/yoruakio/gowebserver/logger"
 )
 
 func main() {
 	config.LoadConfig()
 
-	fmt.Println(config.GetConfig())
+	logger.Infof("Host: %s", config.GetConfig().Host)
 }
